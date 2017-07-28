ee310f574bef7effb5242776f292b1f1c939e8c8
SethTolkamp <stolkamp@uvic.ca>
2017-07-28 15:35:49 -0700

    worker all fix fo sho

--- tests/test_mothership_basic.py
+++ tests/test_mothership_basic.py
@@ -2,23 +2,8 @@
 import unittest
 
 from mothership.base import MothershipServer
-from workers.basic_worker import BasicUserParseWorker
+
 
 
 class TestMothershipBasic(unittest.TestCase):
-"""
-	def test_mothership_initialization(self):
-		#test instansiation of mothership
-		x = 'data'
-
-		originalTarget = 12
-		mom = MothershipServer()
-		mom.port = 5000
-		mom.host = "localhost"
-		worker = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
-		worker.send_to_mother(x,originalTarget)
-
-	def test_mothership_thing(self):
-		x = 1
-	
-"""
+	pass
