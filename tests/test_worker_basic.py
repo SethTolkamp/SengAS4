9ba5cbb2ad42f1a35b4dd55f7df58d48fccd51e2
SethTolkamp <stolkamp@uvic.ca>
2017-07-28 16:03:39 -0700

    fix pop

--- tests/test_worker_basic.py
+++ tests/test_worker_basic.py
@@ -72,6 +72,7 @@ class TestWorkerBasic(unittest.TestCase):
 
         self.assertEqual(len_to_crawl_after, len_to_crawl_before)
 
+# check max length
     def test_worker_add_links_maxLength(self):
         worker = None
         worker = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
@@ -81,22 +82,14 @@ class TestWorkerBasic(unittest.TestCase):
 
         self.assertEqual(len(worker.to_crawl, worker.max_links)
 
+# test list
     def test_worker_add_links_remove_link(self):
         
         worker.to_crawl = []
         worker = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
         worker.to_crawl.append(["https://www.reddit.com/user/Chrikelnel"])
-        worker.to_crawl.pop
+        worker.to_crawl.pop()
  
 
         self.assertEqual(len(len(worker.tocrawl, 0)
-
-
-    
-
-
-
-
-
-
 
