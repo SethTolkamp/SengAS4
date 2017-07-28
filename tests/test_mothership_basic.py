0c075052dbb8ed456b7e8871e1f635b84f1bbbed
SethTolkamp <stolkamp@uvic.ca>
2017-07-28 14:50:47 -0700

    mothership added

--- tests/test_mothership_basic.py
+++ tests/test_mothership_basic.py
@@ -2,14 +2,18 @@
 import unittest
 
 from mothership.base import MothershipServer
+from workers.basic_worker import BasicUserParseWorker
 
 
 class TestMothershipBasic(unittest.TestCase):
 
 	def test_mothership_initialization(self):
-
+		#test instansiation of mothership
 		x = 'data'
 		mom = MothershipServer()
 		worker = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
 		worker.send_to_mother(worker,x,mom)
-		#test instansiation of mothership
+
+	def test_mothership_thing(self):
+		x = 1
+		
